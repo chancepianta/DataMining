4628c665bac3b72df24aa5016aea91e5a988141e
chance <chance.pianta@triumvircorp.com>
2015-04-05 20:54:33 -0500

    commit

--- spamClassification.R
+++ spamClassification.R
@@ -2,17 +2,20 @@ library(e1071)
 
 load("/home/vmadmin/Descargas/spam.Rdata")
 
-train = subset(spam, test == FALSE)$spam
-test = subset(spam, test == TRUE)$spam
+train = subset(spam, test == FALSE)
+test = subset(spam, test == TRUE)
+
+train$test <- NULL
+test$test <- NULL
 
 # Naive Bayes Classifier - Binarized Spam Data
-binaryClassifier <- naiveBayes(train, train)
+binaryClassifier <- naiveBayes(spam ~ ., train)
 binaryPred <- predict(binaryClassifier, test)
-binaryPredTable = table(binaryPred, test)
+binaryPredTable = table(binaryPred, test$spam)
 
 binaryClassifier
 binaryPredTable
 
-CrossTable(binaryPred, test, prop.chisq = FALSE, prop.t = FALSE, dnn = c('predicted', 'actual'))
+CrossTable(binaryPred, test$spam, prop.chisq = FALSE, prop.t = FALSE, dnn = c('predicted', 'actual'))
 
 # Naive Bayes Classifier - Real Valued Attributes
